8c88b66ac4c91b5b7971f9a4fb68adda68e9ebf5
PasserBo <bobo.yiyangxu@gmail.com>
2025-02-23 23:57:38 -0600

    Test Address Catching

--- content.js
+++ content.js
@@ -1,2 +1,9 @@
 console.log("Chrome");
 document.body.style.border = "5px solid red";
+// content.js
+function extractAddressFromPage() {
+    let addressElement = document.querySelector(".property-address, .house-location, .property-detail");
+    return addressElement ? addressElement.innerText.trim() : null;
+}
+
+
